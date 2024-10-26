8c02afd4d6784fb8946ac5f8a211f2e2267b6ec9
elenaludina0573 <elenaludina0@gmail.ru>
2024-10-26 18:19:26 +0300

    Validators posts

---
+++ posts/validators.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from rest_framework.serializers import ValidationError
+
+forbidden_words = [
+    'ерунда', 'глупость', 'чепуха'
+]
+
+
+class TitleValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        if any(word in value for word in forbidden_words):
+            raise ValidationError(f"Заголовок {value} содержит запрещенные слова.")
+
+
+class AuthorValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        value = datetime.strptime(value, '%Y-%m-%d')
+        today = datetime.today()
+        age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+        if age < 18:
+            raise ValidationError(f"Автор должен быть не моложе 18 лет.")
